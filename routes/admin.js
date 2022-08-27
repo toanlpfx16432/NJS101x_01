1e9c658187783563c78b40ddf34860d6ccdfb21d
Toan <toanlpfx16432@funix.edu.vn>
2022-08-27 09:46:47 +0700

    filtering path

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product',(req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/addproduct" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 router.post('/product',(req, res, next)=>{
