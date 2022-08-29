92dc830d9396808e61968f5f197d80e099ddf759
Toan <toanlpfx16432@funix.edu.vn>
2022-08-29 10:12:39 +0700

    Finishing the Controllers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,8 +19,6 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use('/',(req, res, next)=>{
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path:''});
-})
+app.use(errorController.get404)
 
 app.listen(3000)
