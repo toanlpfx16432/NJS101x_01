00ac2d9e8c884404e40f1f36aff25d02854c0b16
Toan <toanlpfx16432@funix.edu.vn>
2022-08-30 11:39:34 +0700

    Editing the Product Data

--- routes/admin.js
+++ routes/admin.js
@@ -18,6 +18,6 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/edit-product => GET
 router.get('/edit-product/:productId',adminController.getEditProduct);
 
-router.post('/edit-product')
+router.post('/edit-product',adminController.postEditProduct);
 
 module.exports = router;
