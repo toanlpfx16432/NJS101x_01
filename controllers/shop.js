a99257a790a9e137f7e23404e6e846c10353a33f
Toan <toanlpfx16432@funix.edu.vn>
2022-08-30 11:07:23 +0700

    Pre-Populating the Edit Product Page with Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
-
 const Cart = require('../models/cart');
+
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -14,12 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-      res.render('shop/product-detail', {
-        product: product,
-        pageTitle: product.title,
-        path: '/products'
-      });
-    });  
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
 }
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -40,7 +40,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
+  Product.findById(prodId, (product)=>{
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
 };
 
