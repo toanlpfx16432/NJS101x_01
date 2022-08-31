95e5c229d4ac5a6a46afcb40a16ae5d3f01b132c
Toan <toanlpfx16432@funix.edu.vn>
2022-08-31 15:08:11 +0700

    Fetching Products

--- util/database.js
+++ util/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'node-complete',
-    password: 'Thinh.med93'
+    database: 'node_complete',
+    password: 'kho12345'
 });
 
 module.exports = pool.promise();
