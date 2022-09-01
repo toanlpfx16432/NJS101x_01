2f46927d01965cd476c8670031949ee10eec434a
Toan <toanlpfx16432@funix.edu.vn>
2022-09-01 10:44:06 +0700

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database')
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,15 +21,38 @@ const res = require('express/lib/response');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use( (req, res, next)=>{
+    User.findByPk(1)
+        .then(user =>{
+            req.user=user;
+            next();
+        })
+        .catch(err=>console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User,{constraint: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
 sequelize
+    //.sync({force: true})
     .sync()
     .then(result =>{
-        console.log(result);
+        return User.findByPk(1);
+        //console.log(result);
+    })
+    .then(user => {
+        if(!user){
+            return User.create({name: 'Thinh', email: 'test@gmail.com'});
+        }
+        return user;
+    })
+    .then(user=>{
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err => console.log(err));
