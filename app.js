0959462ee80415641b5fd9e43ac038297a24350e
Toan <toanlpfx16432@funix.edu.vn>
2022-09-01 09:15:17 +0700

    Syncing JS Definitions to the Database

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database')
+const sequelize = require('./util/database')
 
 const app = express();
 
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const res = require('express/lib/response');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,4 +24,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+    .sync()
+    .then(result =>{
+        console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
--- util/database.js
+++ util/database.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node-complete', 'root', 'Thinh.med93', {dialect: 'mysql', host: 'localhost'});
+const sequelize = new Sequelize('node_complete', 'root', 'kho12345', {dialect: 'mysql', host: 'localhost'});
 
 module.exports = sequelize; 
