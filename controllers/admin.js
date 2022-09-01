11475ef321535d37e00b82a504fae7308325241b
Toan <toanlpfx16432@funix.edu.vn>
2022-09-01 09:55:40 +0700

    Fetching Admin Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -62,11 +62,11 @@ exports.postDeleteProduct = (req, res, next) =>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
+  Product.findAll().then(products => {
+      res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  }).catch(err => console.log(err));
 };
