05c97c4de93237744afff107b78c65210e241cc4
Toan <toanlpfx16432@funix.edu.vn>
2022-09-02 10:07:07 +0700

    Storing Cartitems as Orderitems

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
-
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -133,6 +133,29 @@ exports.postCartDeleteProduct = (req, res, next)=>{
     res.redirect('/cart');
   });
   */
+}
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts()
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(products.map(product =>{
+            product.orderItem = {quantity: product.cartItem.quantity};
+            return product
+          }))
+        })
+        .then(result => {
+          res.redirect('/orders')
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getOrders = (req, res, next) => {
