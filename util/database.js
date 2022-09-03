73b883e231c5feb096efd345c4fbbd765d1f37c8
Toan <toanlpfx16432@funix.edu.vn>
2022-09-03 10:47:26 +0700

    update

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) =>{
-  MongoClient.connect('mongodb+srv://thinhtran:pass123@cluster0.njciaw0.mongodb.net/?retryWrites=true&w=majority')
+  MongoClient.connect('mongodb+srv://toan:kho123@cluster0.5y31ds2.mongodb.net/?retryWrites=true&w=majority')
   .then(result => {
     console.log('Connected!');
     callback(result);
