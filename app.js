812473a820bc8149760d90e22ab8c25a206cbaab
Toan <toanlpfx16432@funix.edu.vn>
2022-09-03 12:28:44 +0700

    Creating Products

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -32,6 +33,5 @@ app.use('/admin', adminRoutes);
 app.use(errorController.get404);
 
 mongoConnect(client =>{
-
     app.listen(3000);
-  })
+})
