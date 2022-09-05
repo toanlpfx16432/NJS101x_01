6ddeffe5ce92fcce2ac5ac1747f67938c7a7a94d
Toan <toanlpfx16432@funix.edu.vn>
2022-09-05 09:23:17 +0700

    Getting Orders

--- models/user.js
+++ models/user.js
@@ -87,10 +87,15 @@ class User {
         );
     });
   }
-  
-  getOder() {
-    const db = getdB();
 
+  getOrders() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .find({
+        'user._id': new ObjectId(this._id)
+      })
+      .toArray()
   }
 
   static findById(userId) {
