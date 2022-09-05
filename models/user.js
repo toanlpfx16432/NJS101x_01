72d1c6cbd8c8c4cfc4f295e0e2e52183e55c2484
Toan <toanlpfx16432@funix.edu.vn>
2022-09-05 09:13:22 +0700

    Adding an Order

--- models/user.js
+++ models/user.js
@@ -61,6 +61,22 @@ class User {
       })  
   }
 
+  addOrder() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then(result => {
+        this.cart = {items: []};
+        const db = getDb();
+        return db
+          .collection('users')
+          .updateOne(
+            {_id: new ObjectId(this._id)}, 
+            {$set: {cart: {items: []}}}
+          );
+      });
+  }v
 
   static findById(userId) {
     const db = getDb();
