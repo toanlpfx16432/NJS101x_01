54271584fdfc0f3c73f501f628d289520373d445
Toan <toanlpfx16432@funix.edu.vn>
2022-09-06 10:41:56 +0700

    Working on the Shopping Cart

--- models/user.js
+++ models/user.js
@@ -13,6 +13,25 @@ const userSchema = new Schema ({
     }
 });
 
+userSchema.methods.addToCart = function(product){
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if(cartProductIndex >= 0){
+    newQuantity = this.cart.items[cartProductIndex].quantity+1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;  
+  }
+  else {
+    updatedCartItems.push({productId: product._id, quantity: 1});
+  }
+  const updatedCart = {items: updatedCartItems};
+  this.cart = updatedCart;
+  return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
