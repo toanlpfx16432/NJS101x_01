c978b2374f3dac653d7c20ff4ddfcb3be70efa24
Toan <toanlpfx16432@funix.edu.vn>
2022-09-06 10:15:44 +0700

    Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -72,7 +72,7 @@ exports.postEditProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
