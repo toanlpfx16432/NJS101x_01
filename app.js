0dbac038c6b9ac2ff77268df2832ed0fd6c91abf
Toan <toanlpfx16432@funix.edu.vn>
2022-09-08 11:10:36 +0700

    Using MongoDB to Store Sessions

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-
+const MongoDBStore = require('connect-mongodb-session')(session);
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const app = express();
+const MONGODB_URI = 'mongodb+srv://toan:kho123@cluster0.5y31ds2.mongodb.net/shop';
 
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session'
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -19,7 +24,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false}));
+app.use(session({secret: 'my secret', resave: false, saveUninitialized: false, store: store}));
 app.use((req, res, next) => {
   User.findById('6316bcd3ee62d8e801c9c5f7')
     .then(user => {
@@ -36,7 +41,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://toan:kho123@cluster0.5y31ds2.mongodb.net/shop?retryWrites=true&w=majority')
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
