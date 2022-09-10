975711c9e0e6c34626d00c5a54d3d26610f0819b
Toan <toanlpfx16432@funix.edu.vn>
2022-09-10 10:08:23 +0700

    Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -38,7 +39,11 @@ User.findOne({email: email})
   if (userDoc) {
     return res.redirect('/signup');
   }
-  const user = new User({email: email, password: password, cart: {items: []}})
+  return bcrypt.hash(password,12);
+
+})
+.then(hashedPassword => {
+  const user = new User({email: email, password: hashedPassword, cart: {items: []}})  
   return user.save();
 })
 .then(result => res.redirect('/login'))
