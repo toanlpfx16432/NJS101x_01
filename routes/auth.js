3167b5a0efa3d666629e6af58da7850d12ca5690
Toan <toanlpfx16432@funix.edu.vn>
2022-09-13 10:00:07 +0700

    Checking For Field Equality

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,11 @@ router.post('/signup',
         return true
     }),
     body('password','Please enter a password with only number and text and at least 5 characters')
-    .isLength({min: 5}).isAlphanumeric()
+    .isLength({min: 5}).isAlphanumeric(),
+    body('confirmPassword').custom((value, {req}) => {
+        if (value !== req.body.password) {throw new Error('Passwords have to match!')}
+        return true
+    })
 ], 
 authController.postSignup);
 
