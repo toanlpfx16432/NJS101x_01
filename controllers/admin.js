2b6afad3b811bdef6c1f17c1314f3101bd4ea0de
Toan <toanlpfx16432@funix.edu.vn>
2022-09-13 20:59:06 +0700

    Validating Product Addition

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,13 @@
 const Product = require('../models/product');
+const { validationResult } = require('express-validator/check');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: false
+    editing: false,
+    hasError: false,
+    errorMessage: null
   });
 };
 
@@ -13,6 +16,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+     console.log(errors.array())
+    return res.status(422).render('admin/edit-product', {
+      path: '/admin/add-product',
+      pageTitle: 'Add Product',
+      editing: false,
+      hasError: true,
+      product: {title: title, imageUrl: imageUrl, price: price, description: description},
+      errorMessage: errors.array()[0].msg
+    });
+  }
   const product = new Product({
     title: title,
     price: price,
@@ -47,7 +62,9 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
         editing: editMode,
-        product: product
+        product: product,
+        hasError: false,
+        errorMessage: null  
       });
     })
     .catch(err => console.log(err));
